3cb3b81d52f28b9051b6313291b9c0fecef26394
ivan.maruca@gmail.com <ivan.maruca@gmail.com>
2018-04-10 21:20:20 +0200

    improve design

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 require "vendor/autoload.php";
 use Graal\Bone\Parser\HtmlParserHtml5;
-use Graal\Bone\Dom\SimplePage;
-$page = new SimplePage("test");
+use Graal\Bone\DOM\Partial;
+use Graal\Bone\DOM\Page;
+use Graal\Bone\DOM\SimplePage;
+use Graal\Bone\Node\HtmlNode;
 
-$html = new HtmlParserHtml5($page->toString());
-echo "$html\n";
+$page = Page::createFromFile("template.html");
+$page->query("#content",0)->setInnerText("Hello Bone !");
+//$html = new HtmlParserHtml5($page->toString());
+//echo "$html\n";
+
+echo "$page\n";
--- src/DOM/PageNode.php
+++ src/DOM/PageNode.php
@@ -3,8 +3,8 @@ namespace Graal\Bone\DOM;
 use Graal\Bone\Node\HtmlNode;
 class PageNode extends HtmlNode{
     var $tag = 'page' ;
-    function __construct(){
-        parent::__construct($this->tag,null);
+    function __construct(HtmlNode $parent = null){
+        parent::__construct($this->tag,$parent);
     }
     public function toString($attributes = true, $recursive = true, $content_only = false){
         return $this->toString_content($attributes,$recursive,$content_only);
